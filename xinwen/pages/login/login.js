919e567d9f47a2543748a96f666446877de77ce1
spiderss <2689592940@qq.com>
2017-10-29 21:50:08 +0800

    login page

---
+++ xinwen/pages/login/login.js
@@ -0,0 +1,40 @@
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+     console.log("obloaler-------");
+  },
+  onReady: function () {
+    console.log("页面已经渲染-------");
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+     console.log("什么时候------");
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+     console.log("页面被后台隐藏----");
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+  
+  loginBtnClick:function(event){
+    console.log(event);
+  }
+
+
+
+
+})
