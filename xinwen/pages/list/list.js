494c27f803d7ffc2a0eb815943ad49d0f45247d5
zhangyl <zhangyulong@llzg.cn>
2017-11-01 09:36:09 +0800

    login list

---
+++ xinwen/pages/list/list.js
@@ -0,0 +1,83 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listType:"news",
+    infos:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that=this;
+    wx.request({
+      url: 'http://localhost/test/little.php',
+      data: {
+        type:"news"
+      },
+      method: "get",
+      success: function (res) {
+        console.log(res.data)
+          that.setData({
+            infos: res.data.results
+         });
+      },
+      fail: function (res) {
+        console.log(res.data);
+        console.log('is failed')
+      }  
+    })     
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
--- xinwen/pages/login/login.js
+++ xinwen/pages/login/login.js
@@ -31,13 +31,45 @@ Page({
 
   },
   
-  loginBtnClick:function(event){
-    console.log(event);
+  loginBtnClick:function(e){
+    var that=this;
+    wx.request({
+      url: '../../testData/login.json',
+      data:{
+        username:this.data.username,
+        password:this.data.password,
+        dosubmit:1
+      },
+      method:"post",
+      success:function(res)
+      {
+        console.log(res.data)
+        try
+        {
+          wx.setStorageSync("id", res);
+        }catch(e){}
+        wx.navigateTo({
+          url: '../user/user',
+        })
+      },
+      fail: function (res) {
+        console.log(res.data);
+        console.log('is failed')
+      }  
+    })
   },  
-  userNameInput:function(evevt)
+  userNameInput:function(e)
   { 
-     var that=this;
-     this.setData({ "userName":'ok'});
+    this.setData({
+      userName:e.detail.value
+    })
+    //console.log(e.detail.value );
+  },
+  passWordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+    //console.log(e.detail.value );
   }
 
 
